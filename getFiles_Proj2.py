8a1e2de71527787ea1c4803599674a2a4af3f3c2
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-15 18:02:59 +0100

    Retrieve file list(i) and files (ii)

--- getFiles_Proj2.py
+++ getFiles_Proj2.py
@@ -4,3 +4,10 @@ url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=resume_
 myFileNames = requests.get(url).text
 
 print(myFileNames)
+
+for i in myFileNames:
+  file_url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=resume_speed&myfilename=' + i
+  myFiles = requests.get(file_url)
+  myFile = open(i, "wb")
+  myFile.write(myFiles.content)
+  myFile.close()
