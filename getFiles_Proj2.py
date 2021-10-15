3288ebee830519c7da4b6596062b1c57640dc3c8
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-15 19:09:58 +0100

    downloading files dataset

--- getFiles_Proj2.py
+++ getFiles_Proj2.py
@@ -5,9 +5,9 @@ myFileNames = requests.get(url).text
 
 print(myFileNames)
 
-for i in myFileNames:
+for i in myFileNames.split(',\n'):
   file_url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=resume_speed&myfilename=' + i
-  iFilePath = '/vnagaraj-files2'+i
+  iFilePath = '/vnagaraj-files2/'+i
   myFiles = requests.get(file_url)
   myFile = open(iFilePath, "wb")
   myFile.write(myFiles.content)
