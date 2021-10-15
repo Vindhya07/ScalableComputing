2cffffe5188519febb6e5bd18c86cb0154605524
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-15 19:06:47 +0100

    creating directory for dataset and downloading files

--- getFiles_Proj2.py
+++ getFiles_Proj2.py
@@ -7,7 +7,7 @@ print(myFileNames)
 
 for i in myFileNames:
   file_url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=resume_speed&myfilename=' + i
-  iFIlePath = '/vnagaraj-files2'+i
+  iFilePath = '/vnagaraj-files2'+i
   myFiles = requests.get(file_url)
   myFile = open(iFilePath, "wb")
   myFile.write(myFiles.content)
