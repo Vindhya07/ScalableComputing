047b66afca84f6f1f02e3eea7cfafabd29d55763
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-15 19:05:39 +0100

    Creating directory for data files and downloading

--- getFiles_Proj2.py
+++ getFiles_Proj2.py
@@ -7,7 +7,8 @@ print(myFileNames)
 
 for i in myFileNames:
   file_url = 'https://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=resume_speed&myfilename=' + i
+  iFIlePath = '/vnagaraj-files2'+i
   myFiles = requests.get(file_url)
-  myFile = open(i, "wb")
+  myFile = open(iFilePath, "wb")
   myFile.write(myFiles.content)
   myFile.close()
