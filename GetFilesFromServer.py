d1944274f058d08de1a16ba769c3c40b48b76c75
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-31 23:31:38 +0000

    get files - folder update

--- GetFilesFromServer.py
+++ GetFilesFromServer.py
@@ -5,6 +5,6 @@ fileList = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj
 
 for i in fileList.split(',\n'):
     f = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=noresume_speed&myfilename="+i)
-    file = open("imglist/"+i, "wb")
+    file = open("vnagaraj-files2/"+i, "wb")
     file.write(f.content)
     file.close()
