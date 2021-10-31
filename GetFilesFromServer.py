871a6f4a850151929bae99d3e1ff8b4ddb592376
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-31 23:45:37 +0000

    try2

--- GetFilesFromServer.py
+++ GetFilesFromServer.py
@@ -6,7 +6,7 @@ fileList = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj
 
 for i in fileList.split(',\n'):
     f = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=noresume_speed&myfilename="+i)
-    os.chdir("/vnagaraj-files2/")
+    os.chdir("vnagaraj-files2")
     file = open(i, "wb")
     file.write(f.content)
     file.close()
