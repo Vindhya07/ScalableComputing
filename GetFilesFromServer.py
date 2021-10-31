9754324fbf5c8da8a1d0597b146ab389f590bc3f
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-31 23:30:59 +0000

    add

---
+++ GetFilesFromServer.py
@@ -0,0 +1,10 @@
+import requests
+
+fileList = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=noresume_speed").text
+
+
+for i in fileList.split(',\n'):
+    f = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=noresume_speed&myfilename="+i)
+    file = open("imglist/"+i, "wb")
+    file.write(f.content)
+    file.close()
