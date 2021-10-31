fecc5d731127d289bdf31b34ce18064779f41625
Vindhya07 <vindhya.n2907@gmail.com>
2021-10-31 23:41:06 +0000

    os chdir change

--- GetFilesFromServer.py
+++ GetFilesFromServer.py
@@ -1,10 +1,12 @@
 import requests
+import os
 
 fileList = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=noresume_speed").text
 
 
 for i in fileList.split(',\n'):
     f = requests.get("http://cs7ns1.scss.tcd.ie/index.php/?shortname=vnagaraj&download=noresume_speed&myfilename="+i)
-    file = open("vnagaraj-files2/"+i, "wb")
+    os.chdir("/vnagaraj-files2/")
+    file = open(i, "wb")
     file.write(f.content)
     file.close()
